d658056891894a931fe6a22599d861acd27ee869
Matheus Daher <matheusdaher@gmail.com>
2025-02-05 08:39:38 -0500

    Refactor LandingPage to use buildClient for API requests and update notes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,30 +1,18 @@
-import axios from 'axios';
-
+import build from "next/dist/build";
+import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
   //console.log(currentUser);
   //axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  };
- 
-  return <h1>Landing Page</h1>;
+  console.log(err.message);
+};
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server
-    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-        },
-      );
-    return data;
-  } else {
-    // we are on the browser
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-}  
+return <h1>Landing Page</h1>;
+
+LandingPage.getInitialProps = async ({ context }) => {
+  const client = buildClient({ context });
+  const { data } = await buildClient({ context }).get("/api/users/currentuser");
+
+  return data;
+};
 
 export default LandingPage;
