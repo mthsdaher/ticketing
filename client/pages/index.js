91cc1a96135c97966f83f7a70c239caf18840ff1
Matheus Daher <matheusdaher@gmail.com>
2025-02-05 08:32:33 -0500

    Update LandingPage to use request headers in getInitialProps and modify notes

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,18 +8,16 @@ const LandingPage = ({ currentUser }) => {
  
   return <h1>Landing Page</h1>;
 
-LandingPage.getInitialProps = async ({  }) => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev',
+        headers: req.headers,
         },
-      }
-    );
+      );
     return data;
   } else {
     // we are on the browser
@@ -28,8 +26,5 @@ LandingPage.getInitialProps = async ({  }) => {
     return data;
   }
 }  
-
-
-
 
 export default LandingPage;
